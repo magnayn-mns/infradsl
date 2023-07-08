a213be706e5c66013a9c785f1d2ed18a8f7e0441
Nigel Magnay <112480361+magnayn-mns@users.noreply.github.com>
2023-07-08 16:06:04 +0100

    Update run.sh

--- scripting-host/src/main/scripts/pulumi/run.sh
+++ scripting-host/src/main/scripts/pulumi/run.sh
@@ -4,6 +4,8 @@ export PATH=$PATH:/root/.pulumi/bin
 
 # TODO: as a parameter
 export DSL_SCRIPT=/github/workspace/script.infra.kts
+export PULUMI_CONFIG_PASSPHRASE=
 
 pulumi login --local
+pulumi stack select foo
 pulumi preview --non-interactive
